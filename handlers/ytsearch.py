f7870ee3518ff413f8157c7ccbae324477937dde
HzJester <86559859+HzJester@users.noreply.github.com>
2021-08-27 00:40:25 +0300

    Update ytsearch.py

--- handlers/ytsearch.py
+++ handlers/ytsearch.py
@@ -16,7 +16,7 @@ import pyrogram
 
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
-@app.on_message(pyrogram.filters.command(["bul","bul@JesterMusicBot"]))
+@app.on_message(pyrogram.filters.command("bul"))
 async def ytsearch(_, message: Message):
     try:
         if len(message.command) < 2:
