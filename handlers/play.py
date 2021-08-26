39198c06e42f9340278dd85f96c34dd0b06e8d3a
HzJester <86559859+HzJester@users.noreply.github.com>
2021-08-27 00:26:59 +0300

    Update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -85,7 +85,7 @@ async def generate_cover(requested_by, title, views, duration, thumbnail):
 
 
 
-@Client.on_message(filters.command(["oynat","oynat@JesterMusicBot"]) 
+@Client.on_message(command(["oynat","oynat@JesterMusicBot"]) 
                    & filters.group
                    & ~filters.edited 
                    & ~filters.forwarded
